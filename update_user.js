0492fc97430aba84e14319da574c0094c5e9c750
cfa532 <cfa532@yahoo.com>
2024-11-13 20:30:40 +0800

    Not sync for now.

--- update_user.js
+++ update_user.js
@@ -8,9 +8,16 @@
         let user = JSON.parse(request["user"])
         let userMid = lapi.MMCreate(authSid, APP_ID, APP_EXT, user.username, 2, 0x07276704)
         let mmsid = lapi.MMOpen(authSid, userMid, "cur")
-        lapi.MiMeiSync(authSid, "", userMid, [])
-        lapi.Set(mmsid, USER_DATA_KEY, user)
-        lapi.MMBackup(authSid, userMid, "", "delref=true")
+        if (!lapi.Get(mmsid, USER_DATA_KEY)) {
+            console.log("Init mid", userMid)
+            lapi.Set(mmsid, USER_DATA_KEY, user)
+            lapi.MMBackup(authSid, userMid, "", "delref=true")
+            lapi.MiMeiPublish(authSid, "", userMid)
+        } else {
+            console.log("Sync mid", userMid)
+            // lapi.MiMeiSync(authSid, "", userMid, {})
+        }
+        console.log(request["user"], userMid)
         return userMid
     } catch(e) {
         console.error("update_user error:", JSON.stringify(request), e)
