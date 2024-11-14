f599913b6bdbaab5eabe0f510ff6570357bc5efe
cfa532 <cfa532@yahoo.com>
2024-11-14 11:44:29 +0800

    MFIsExist, logout

--- update_user.js
+++ update_user.js
@@ -1,5 +1,6 @@
 ((request, args)=>{
-    // Update user data. Usually after login.
+    // Update user data upon login. Authentication server will provide username.
+    // create local user mid with the given username.
     try {
         const USER_DATA_KEY = "user_data_key"
         const APP_ID = request["aid"]       // App ID assigned by Leither upon publication
@@ -8,14 +9,15 @@
         let user = JSON.parse(request["user"])
         let userMid = lapi.MMCreate(authSid, APP_ID, APP_EXT, user.username, 2, 0x07276704)
         let mmsid = lapi.MMOpen(authSid, userMid, "cur")
-        if (!lapi.Get(mmsid, USER_DATA_KEY)) {
+        if (!lapi.MFIsExist(mmsid, userMid)) {
             console.log("Init mid", userMid)
             lapi.Set(mmsid, USER_DATA_KEY, user)
             lapi.MMBackup(authSid, userMid, "", "delref=true")
             lapi.MiMeiPublish(authSid, "", userMid)
+            console.log(userMid, "inited.")
         } else {
-            console.log("Sync mid", userMid)
-            // lapi.MiMeiSync(authSid, "", userMid, {})
+            console.log("Syncing mid", userMid)
+            // lapi.MiMeiSync(mmsid, "", userMid, {})
         }
         console.log(request["user"], userMid)
         return userMid
